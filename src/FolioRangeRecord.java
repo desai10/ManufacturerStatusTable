682bbd28b55fcdbe50c2ba4a7c532bea38cab839
vaagul <vaagul@gmail.com>
2018-08-10 11:10:08 +0530

    Added Skeleton

--- src/FolioRangeRecord.java
+++ src/FolioRangeRecord.java
@@ -1,2 +1,24 @@
 public class FolioRangeRecord {
+    int rangeStart,rangeEnd,transferCode;
+    char statusCode;
+
+    public FolioRangeRecord(int rangeStart,int rangeEnd,char statusCode,int transferCode) {
+        this.rangeStart = rangeStart;
+        this.rangeEnd=rangeEnd;
+        this.statusCode=statusCode;
+        this.transferCode=transferCode;
+    }
+
+    public int isContainsRange(FolioRangeRecord record){
+        //paramet is new input
+        return 0;
+    }
+
+    public String convertRecord2String(FolioRangeRecord record){
+        return new String();
+        // format is "A\tB\tS\tT"
+    }
+
+
+
 }
--- src/StatusTable.java
+++ src/StatusTable.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatusTable {
+    List<FolioRangeRecord> statusTableList = new ArrayList<>();
+
+    public void addInputRecord(FolioRangeRecord record){
+        return;
+    }
+
+    public void printTable(){
+        //call record.convertRecord2String
+    }
+
+    public void refineStatusTable(){
+        //sort it and check all the test cases
+    }
+
 }
